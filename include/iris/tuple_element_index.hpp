8f46ec6530775db4ef27aaccd4c8533fbbcc7451
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-12-19 22:59:40 +0900

    Add tuple_element_index

---
+++ include/iris/tuple_element_index.hpp
@@ -0,0 +1,58 @@
+#include <tuple>
+#include <type_traits>
+#include <variant>
+
+namespace iris::detail {
+  template <std::size_t I, class T>
+  struct indexed {};
+
+  template <class Seq, class... Ts>
+  struct indexer;
+
+  template <std::size_t... Is, class... Ts>
+  struct indexer<std::index_sequence<Is...>, Ts...> : indexed<Is, Ts>... {};
+
+  template <class T, std::size_t I>
+  std::integral_constant<std::size_t, I>
+  get_index(const indexed<I, T>&); // undefined
+
+  // tuple_element_index
+
+  template <class T, class Tuple>
+  struct tuple_element_index;
+
+  template <class T, class Tuple>
+  struct tuple_element_index<T, const Tuple> : tuple_element_index<T, Tuple> {};
+
+  template <class T, class... Ts>
+  struct tuple_element_index<T, std::tuple<Ts...>>
+    : decltype(get_index<T>(indexer<std::index_sequence_for<Ts...>, Ts...>{})) {
+  };
+
+  template <class T, class Tuple>
+  inline constexpr std::size_t tuple_element_index_v =
+    tuple_element_index<T, Tuple>::value;
+
+  // variant_alternative_index
+
+  template <class T, class Variant>
+  struct variant_alternative_index;
+
+  template <class T, class Variant>
+  struct variant_alternative_index<T, const Variant>
+    : variant_alternative_index<T, Variant> {};
+
+  template <class T, class... Ts>
+  struct variant_alternative_index<T, std::variant<Ts...>>
+    : decltype(get_index<T>(indexer<std::index_sequence_for<Ts...>, Ts...>{})) {
+  };
+
+  template <class T, class Variant>
+  inline constexpr std::size_t variant_alternative_index_v =
+    variant_alternative_index<T, Variant>::value;
+} // namespace iris::detail
+
+namespace iris {
+  using detail::tuple_element_index, detail::tuple_element_index_v;
+  using detail::variant_alternative_index, detail::variant_alternative_index_v;
+} // namespace iris
--- tests/main/main.cpp
+++ tests/main/main.cpp
@@ -1,6 +1,18 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iris/fundamental.hpp>
+#include <iris/tuple_element_index.hpp>
 
 TEST_CASE("main", "[main][similar_to]") {
   CHECK(iris::similar_to<const int&, int>);
+}
+
+TEST_CASE("main", "[main][tuple_element_index]") {
+  // clang-format off
+  CHECK(iris::tuple_element_index_v<int, std::tuple<int, double, std::monostate>> == 0);
+  CHECK(iris::tuple_element_index_v<double, std::tuple<int, double, std::monostate>> == 1);
+  CHECK(iris::tuple_element_index_v<std::monostate, std::tuple<int, double, std::monostate>> == 2);
+  CHECK(iris::variant_alternative_index_v<int, std::variant<int, double, std::monostate>> == 0);
+  CHECK(iris::variant_alternative_index_v<double, std::variant<int, double, std::monostate>> == 1);
+  CHECK(iris::variant_alternative_index_v<std::monostate, std::variant<int, double, std::monostate>> == 2);
+  // clang-format on
 }
